8c4adb2570069229e1ed66e2b53dddd5bb2e0d95
Alp Arslan Aziz <aaziz3@worldbank.org>
2019-03-22 11:00:14 -0400

    fixed lookups

--- data-raw/add_internal_data.R
+++ data-raw/add_internal_data.R
@@ -1,8 +1,10 @@
 library(dplyr)
 library(jsonlite)
 
+production_root_url <- 'https://datacatalog.worldbank.org'
+
 # Metadata from flat files
-ddh_lovs_df           <- ddhconnect::get_lovs(root_url = dkanr::get_url())
+ddh_lovs_df           <- ddhconnect::get_lovs(root_url = production_root_url)
 
 eex_ddh_vocab_df      <- readxl::read_excel("./data-raw/controlled_vocab_mapping.xlsx")
 basic_json_mapping    <- readxl::read_excel("./data-raw/eex_ddh_JSON_lookup_basic.xlsx")
@@ -23,17 +25,11 @@ assertthat::assert_that(nrow(invalid_controlled_vocab) == 0, msg = 'Invalid valu
 assertthat::assert_that(nrow(invalid_complex) == 0, msg = 'Invalid values present in complex_lookup')
 assertthat::assert_that(nrow(invalid_constant) == 0, msg = 'Invalid values present in constant_lookup')
 
-# Add tids
-eex_ddh_vocab_df     <- eex_ddh_vocab_df %>% left_join(ddh_lovs_df, by = c("machine_name","list_value_name"))
-constant_lookup      <- constant_lookup %>%  left_join(ddh_lovs_df, by = c("machine_name","list_value_name"))
+# Merge controlled_vocab and constant lookups
 master_basic_lookup  <- rbind(eex_ddh_vocab_df, constant_lookup)
 
-
-# Check for NA values
-na_tid <- master_basic_lookup %>% filter((machine_name != "field_wbddh_dsttl_upi" & machine_name != "field_wbddh_collaborator_upi") & is.na(tid))
-assertthat::assert_that(nrow(na_tid) == 0, msg = 'Invalid values present in complex_json_mapping')
-
 # Add JSON fields
+# The rows with NA as JSON fields have the constant metadata values
 basic_json_mapping   <- basic_json_mapping %>% select(machine_name, eex_field_JSON)
 master_basic_lookup  <- master_basic_lookup %>% left_join(basic_json_mapping, by = "machine_name")
 
