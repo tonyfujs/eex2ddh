889b5e2d541de42b7b22c2960e9c0013b65e43b4
Alp Arslan Aziz <aaziz3@worldbank.org>
2019-03-22 11:02:27 -0400

    cleaned code

--- data-raw/add_internal_data.R
+++ data-raw/add_internal_data.R
@@ -1,11 +1,13 @@
 library(dplyr)
 library(jsonlite)
 
+# Set production URL
 production_root_url <- 'https://datacatalog.worldbank.org'
 
-# Metadata from flat files
+# Get ddh lov fields
 ddh_lovs_df           <- ddhconnect::get_lovs(root_url = production_root_url)
 
+# Metadata from flat files
 eex_ddh_vocab_df      <- readxl::read_excel("./data-raw/controlled_vocab_mapping.xlsx")
 basic_json_mapping    <- readxl::read_excel("./data-raw/eex_ddh_JSON_lookup_basic.xlsx")
 complex_lookup        <- readxl::read_excel("./data-raw/eex_ddh_JSON_lookup_complex.xlsx")
@@ -19,7 +21,6 @@ invalid_complex           <- complex_lookup %>% filter(machine_name != "field_ex
 invalid_constant  <- constant_lookup %>% 
   filter(machine_name != "field_wbddh_dsttl_upi" & machine_name != "field_wbddh_collaborator_upi") %>% 
   anti_join(ddh_lovs_df, by = "machine_name") 
-
 
 assertthat::assert_that(nrow(invalid_controlled_vocab) == 0, msg = 'Invalid values present in eex_ddh_vocab_df')
 assertthat::assert_that(nrow(invalid_complex) == 0, msg = 'Invalid values present in complex_lookup')
