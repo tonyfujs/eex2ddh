ef96d7a459343d4b1a3aa83b62e1c74c10081f6c
Alp Arslan Aziz <aaziz3@worldbank.org>
2019-04-05 16:40:02 -0400

    quick fix

--- R/update_existing_dataset.R
+++ R/update_existing_dataset.R
@@ -54,7 +54,7 @@ update_existing_dataset <- function(metadata_list,
   
   resource_nid <- ddhconnect::get_resource_nids(metadata_dataset_ddh)
     
-  update_resources(resp_dat$nid, resource_nid, metadata_resources)
+  update_resources_2(resp_dat$nid, resource_nid, metadata_resources)
   
   # test created dataset
   metadata_dataset_test <- ddhconnect::get_metadata(nid = resp_dat$nid,
