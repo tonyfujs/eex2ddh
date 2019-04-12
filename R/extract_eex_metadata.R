e4780746010bdcda9869c467bcadc447223aff20
Alp Arslan Aziz <aaziz3@worldbank.org>
2019-04-11 21:37:47 -0400

    fixed format mapping

--- R/extract_eex_metadata.R
+++ R/extract_eex_metadata.R
@@ -9,8 +9,19 @@
 
 extract_eex_metadata <- function(eex_internal_id) {
   
-  url      <- paste0("https://energydata.info/api/3/action/package_show?id=", eex_internal_id)
-  eex_data <- jsonlite::fromJSON(url, simplifyVector = FALSE)
+  #TODO Revert back to logic used for PROD
+  # url      <- paste0("https://energydata.info/api/3/action/package_show?id=", eex_internal_id)
+  # eex_data <- jsonlite::fromJSON(url, simplifyVector = FALSE)
+  
+  url   <- paste0("https://energydata.staging.derilinx.com/api/3/action/package_show?id=", eex_internal_id)
+  
+  # Send request to API
+  resp  <- httr::GET(url,
+                     httr::add_headers(.headers = c('X-API-KEY' = "2d451b91-95d0-4177-98fc-a4e3b0a4fd12",
+                                                    'charset' = "utf-8")),
+                     httr::accept_json())
+  
+  eex_data <- jsonlite::fromJSON(httr::content(resp, "text"), simplifyVector = FALSE)
   
   return(eex_data$result)
 }
